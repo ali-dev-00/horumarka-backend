b05faa75736c9ee492f4b730d7b5c06efc6d0f86
Ali Dev <mirzaaly8@gmail.com>
2025-08-31 21:36:33 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const frontendUrl = process.env.FRONTEND_URL || 'https://company-site-beryl.vercel.app/';
+  const frontendUrl = process.env.FRONTEND_URL || 'https://company-site-beryl.vercel.app';
   const port = configService.get<number>('PORT') || 3001;
   
   console.log('frontendUrl',frontendUrl)
